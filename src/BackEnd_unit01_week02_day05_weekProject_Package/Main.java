dbff31ebb1a30857d4440f5a4b345b7adaf44e26
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-07 11:28:38 +0200

    Commit, project created

---
+++ src/BackEnd_unit01_week02_day05_weekProject_Package/Main.java
@@ -0,0 +1,5 @@
+package BackEnd_unit01_week02_day05_weekProject_Package;
+
+public class Main {
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module BackEnd_unit01_week02_day05_weekProject {
+}

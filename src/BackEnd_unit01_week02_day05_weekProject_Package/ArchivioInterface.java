8dff53fb95919c6a6b5e8410eeaae3325a54ccbb
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-07 12:51:34 +0200

    Commit, "ArchivioInterface" deleted

--- src/BackEnd_unit01_week02_day05_weekProject_Package/ArchivioInterface.java
+++
@@ -1,5 +0,0 @@
-package BackEnd_unit01_week02_day05_weekProject_Package;
-
-public interface ArchivioInterface {
-
-}
--- src/BackEnd_unit01_week02_day05_weekProject_Package/Main.java
+++ src/BackEnd_unit01_week02_day05_weekProject_Package/Main.java
@@ -1,5 +1,23 @@
 package BackEnd_unit01_week02_day05_weekProject_Package;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+	// - - - - - - - - - - - - - - - - - - - - ArrayList "archivio" definition
+	public static List<Elemento> archivio = new ArrayList<>();
+
+	// - - - - - - - - - - - - - - - - - - - - main method definition
+	public static void main(String[] args) {
+
+		// - - - - - - - - - - - - - - - - - - - - sample objects creation
+		// public Libro(String _isbn, String _titolo, int _anno, int _pagine, String
+		// _autore, String _genere)
+		Libro libro01 = new Libro("", "", 2010, 100, "", "");
+		// public Rivista(String _isbn, String _titolo, int _anno, int _pagine,
+		// Periodicita _periodicita)
+
+	}
 
 }
